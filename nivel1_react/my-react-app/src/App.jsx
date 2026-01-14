70e7ad633b1861cb78f6b0a3623890fb30e5f8a9
Estefania <estefaniacastellanos802@gmail.com>
2026-01-14 17:06:40 +0100

    Parte 2

--- nivel1_react/my-react-app/src/App.jsx
+++ nivel1_react/my-react-app/src/App.jsx
@@ -1,4 +1,45 @@
-function App() {
-  return <h1>Nivel 1 de React desbloqueado</h1>
+export default function App() {
+  //App renderiza a ParentComponent
+  return <ParentComponent />;
 }
-export default App
+
+function ParentComponent() {
+  //ParentComponent renderiza los tres en orden
+  return (
+    <>
+      <UserComponent />
+      <ProfileComponent />
+      <FeedComponent />
+    </>
+  );
+}
+function UserComponent() {
+  return (
+    <div>
+      <h2>Nombre: Estefania Castellanos</h2>
+      <p>Estado: En línea</p>
+    </div>
+  );
+}
+
+function ProfileComponent() {
+  return (
+    <div>
+      <h2>Perfil de Usuario</h2>
+      <p>Bio: Hola Buenos dias.</p>
+    </div>
+  );
+}
+
+function FeedComponent() {
+  return (
+    <div>
+      <h2>Últimas publicaciones</h2>
+      <ul>
+        <li>Aprendiendo componentes en React</li>
+        <li>Mi primer proyecto con Vite</li>
+        <li>Holi klara</li>
+      </ul>
+    </div>
+  );
+}
